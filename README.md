5387eefe05683e6dc94b316016a345d010e769aa
Gabriel Nilli <gabbonilli@gmail.com>
2024-05-15 23:28:49 +0200

    Added contents, return button, links

--- README.md
+++ README.md
@@ -2,7 +2,9 @@
 
 ## DA SISTEMARE
 
-- Posizione nome e descrizione
 - Grafico deve essere grande al centro
 - Grafico in generale
 - Aggiungere contenuti
+- Progress bars debono essere 3 a destra e 3 a sinistra
+- Scritte sotto progress bars adattabili anche su telefono
+- Aggiustare padding a destra
--- general-scripts/script.js
+++ general-scripts/script.js
@@ -1,46 +1,15 @@
-//---------------------------------------------------\ 
-// Script for skills level                            |
-// --------------------------------------------------/
-// Load the Visualization API and the corechart package.
-google.charts.load('current', {packages: ['corechart', 'bar']});
+// script for the animation of the return button
+window.addEventListener('scroll', function() {
+    var scrollPercent = (document.documentElement.scrollTop + document.body.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight) * 100;
+    
+    if (scrollPercent > 40) {
+      var returnButton = document.getElementById('return-button');
+      returnButton.style.display = 'block';
+      returnButton.style.animation = 'slideInRight 0.5s ease forwards';
+    } else {
+      document.getElementById('return-button').style.display = 'none';
+    }
+  });
 
-// Set a callback to run when the Google Visualization API is loaded.
-google.charts.setOnLoadCallback(drawSkillsChart);
 
-// Callback that creates and populates a data table,
-// instantiates the bar chart, passes in the data and
-// draws it.
-function drawSkillsChart() {
-
-    // Create the data table.
-    var data = google.visualization.arrayToDataTable([
-        ['Skill', 'Level', { role: 'style' }],
-        ['C++   ', 90, '#FFD700'],
-        ['HTML   ', 80, '#FFD700'],
-        ['CSS   ', 80, '#FFD700'],
-        ['JavaScript   ', 60, '#FFD700'],
-        ['PHP   ', 50, '#FFD700'],
-        ['SQL/NoSQL   ', 50, '#FFD700']
-    ]);
-
-    // Set chart options
-    var options = {
-        title: 'My Skills',
-        chartArea: { width: '70%', backgroundColor: '#053F5E' },
-        backgroundColor: '#053F5E',
-        hAxis: {
-            minValue: 0,
-            maxValue: 100,
-            title: 'Proficiency Level'
-        },
-        vAxis: {
-            title: ''
-        },
-        bars: 'horizontal', // Required for Material Bar Charts.
-        legend: { position: 'none' }
-    };
-
-    // Instantiate and draw our chart, passing in some options.
-    var chart = new google.visualization.BarChart(document.getElementById('chart_div'));
-    chart.draw(data, options);
-}
+// script for the animation of the gif text
